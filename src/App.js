a960780b55bb4981ba40727ed4d64826bc5b16d7
Joseph Lynn <josephlynn94@gmail.com>
2021-02-25 17:14:17 -0500

    setup initaly project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="app-wrapper">
+                <h1>Budget App</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App
